243351ebf4c40f99fcb8487fa39eaa653db523d7
Ben-yi <banqhsia@msn.com>
2016-11-18 21:18:27 +0800

    Add Google Analytics code

--- js/app.js
+++ js/app.js
@@ -56,3 +56,11 @@ TRAExt.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, 
 
 // Initialize moment.js locale `zh-tw`
 moment.locale('zh-tw');
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-40661556-5', 'auto');
+ga('send', 'pageview');
