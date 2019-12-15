ed37cbbcb796c07f3a9a9da4e227cdedfa96ff2a
Benyi <banqhsia@msn.com>
2019-12-15 14:49:23 +0800

    新增 TrainItem，複刻 search 頁面的火車 row

---
+++ src/components/timetable/TrainItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <tbody>
+    <td>
+      <div class="ui grid">
+        <div class="row">
+          <!-- 車種 -->
+          <div class="sixteen wide column">
+            <h3 class="ui header">
+              {{ train.TrainInfo.TrainTypeName.Zh_tw }}
+              <h5 class="ui right floated header">
+                <div class="sub header">{{ train.TrainInfo.TrainNo }}</div>
+              </h5>
+            </h3>
+          </div>
+
+          <!-- 終點站 -->
+          <div class="sixteen wide column">
+            <h5 class="ui header">
+              <div class="sub header">
+                <i class="arrow right icon inline-icon"></i>
+              </div>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </td>
+
+    <!-- 經由 -->
+    <td class="center aligned">
+      <div
+        class="ui circular basic label"
+        :class="[tripLine(train.TrainInfo.TripLine, true)]"
+        v-if="train.TrainInfo.TripLine"
+      >{{ tripLine(train.TrainInfo.TripLine) }}</div>
+    </td>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: ["train"],
+  data() {
+    return {
+      // trains: trains
+    };
+  },
+  methods: {}
+};
+</script>
