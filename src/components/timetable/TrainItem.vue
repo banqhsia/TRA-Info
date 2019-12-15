7b892bd796b6ca0680b86a07774984eab20965d3
Benyi <banqhsia@msn.com>
2019-12-15 15:50:59 +0800

    將 TrainItem 的功能完整搬進來，修正欄位錯誤

--- src/components/timetable/TrainItem.vue
+++ src/components/timetable/TrainItem.vue
@@ -17,7 +17,9 @@
           <div class="sixteen wide column">
             <h5 class="ui header">
               <div class="sub header">
+                {{ train.TrainInfo.StartingStationName.Zh_tw }}
                 <i class="arrow right icon inline-icon"></i>
+                {{ train.TrainInfo.EndingStationName.Zh_tw }}
               </div>
             </h5>
           </div>
@@ -32,6 +34,42 @@
         :class="[tripLine(train.TrainInfo.TripLine, true)]"
         v-if="train.TrainInfo.TripLine"
       >{{ tripLine(train.TrainInfo.TripLine) }}</div>
+    </td>
+
+    <!-- 出發時間 -->
+    <td class="center aligned departure-time">{{ train.StopTimes[0].DepartureTime }}</td>
+    <!-- 途經 -->
+    <td class="center aligned">
+      <h5 class="ui trip icon header">
+        <i class="arrow right icon"></i>
+        <div
+          class="sub header"
+        >{{ timeDiff( train.StopTimes[0].DepartureTime, train.StopTimes[1].ArrivalTime).humanize }}</div>
+      </h5>
+    </td>
+
+    <!-- 抵達時間 -->
+    <td class="center aligned">{{ train.StopTimes[1].ArrivalTime }}</td>
+
+    <!-- 說明 -->
+    <td>
+      <div
+        class="ui basic grey horizontal medium label"
+      >{{ train.StopTimes[1].StopSequence - train.StopTimes[0].StopSequence }}站</div>
+
+      <div class="ui blue horizontal medium label" v-if="train.TrainInfo.WheelChairFlag">輪椅</div>
+
+      <div class="ui pink horizontal medium label" v-if="train.TrainInfo.BreastFeedFlag">哺乳室</div>
+
+      <div class="ui green horizontal medium label" v-if="train.TrainInfo.BikeFlag">自行車</div>
+
+      <div class="ui yellow horizontal medium label" v-if="DMULabel(train.TrainInfo.Note.Zh_tw)">柴聯</div>
+
+      <div
+        class="ui brown horizontal medium label"
+        v-if="acrossDayLabel(train.TrainInfo.Note.Zh_tw)"
+      >跨日</div>
+      {{ train.TrainInfo.Note | noteFormat }}
     </td>
   </tbody>
 </template>
